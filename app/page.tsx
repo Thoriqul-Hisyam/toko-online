aa8ca0021c495ec563dc4ade14288ee38c0051f0
Thoriqul-Hisyam <thoriqulh12@gmail.com>
2023-11-15 09:53:33 +0700

    feat:(add banner home statis)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Container from "./components/Container";
+import HomeBanner from "./components/HomeBanner";
 
 
 export default function Home() {
   return (
-    <div> ShyShop</div>
+    <div className="p-8">
+      <Container>
+        <div>
+          <HomeBanner />
+        </div>
+      </Container>
+    </div>
   )
 }
